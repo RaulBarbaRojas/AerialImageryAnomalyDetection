b5ca81dec7cd33e2e9aa509f07b48fa2c86ba84a
PolarNebula <polarnebula48@gmail.com>
2025-02-04 17:53:48 +0100

    Added ziz model

--- src/aerial_anomaly_detection/models/ziz.py
+++ src/aerial_anomaly_detection/models/ziz.py
@@ -1,5 +1,5 @@
 """
-Module to implement the izi architecture for anomaly detection on aerial imagery.
+Module to implement the ziz architecture for anomaly detection on aerial imagery.
 
 Cite: Contreras-Cruz, M. A., Correa-Tome, F. E., Lopez-Padilla, R., & Ramirez-Paredes, J. P. (2023).\
 Generative Adversarial Networks for anomaly detection in aerial images. Computers and Electrical Engineering, 106, 108470.
@@ -14,26 +14,26 @@ import torch
 from aerial_anomaly_detection.models.autoencoder import AutoEncoder
 
 
-class Izi(AutoEncoder):
+class Ziz(AutoEncoder):
     """
-    A class to implement the izi architecture.
+    A class to implement the ziz architecture.
     """
 
 
     def __init__(self, latent_dimension : int, img_width : int, img_height : int,
                  pretrained_decoder_weights_path : str | Path , **model_settings : Dict[str, Any]) -> None:
         """
-        Constructor method of the Izi class.
+        Constructor method of the Ziz class.
 
         Args:
-            latent_dimension (int): the size of the latent dimension of the izi architecture.
+            latent_dimension (int): the size of the latent dimension of the ziz architecture.
             img_width (int): the width of the input (and output) images.
             img_height (int): the height of the input (and output) images.
             pretrained_decoder_weights_path (str | Path): the pre-trained weights of the decoder.
             model_settings (Dict[str, Any]): a dictionary containing other settings relevant for using the model in the general workflow.
         """
 
-        # Step 1: Setting up the Izi architecture
+        # Step 1: Setting up the Ziz architecture
         super().__init__(latent_dimension = latent_dimension,
                          img_width = img_width,
                          img_height = img_height,
@@ -44,3 +44,19 @@ class Izi(AutoEncoder):
 
         for parameter in self.decoder.parameters():
             parameter.requires_grad = False
+
+
+    def forward(self, x : torch.Tensor) -> torch.Tensor:
+        """
+        Method to implement the forward pass of the Ziz architecture.
+
+        Args:
+            x (torch.Tensor): the input tensor of the ziz architecture.
+
+        Returns:
+            The output tensor after the forward pass of the ziz architecture.
+        """
+        x = self.decoder(x)
+        x = self.encoder(x)
+
+        return x
